6113e9e6da687eb7c4327c4b1d32319457165bb2
Viktoria <zaharova181114@gmail.com>
2021-11-16 13:59:20 +0200

    Initial commit

---
+++ app/js/map.js
@@ -0,0 +1,42 @@
+// Инициализация карты
+ymaps.ready(init);
+
+function init () {
+
+    //Центрирование и выбор масштаба карты
+    var myMap = new ymaps.Map('map', {
+        center: [55.857508, 37.583203],
+        zoom: 16
+    });
+
+    // Создание своей метки
+    var myPlacemark = new ymaps.Placemark(
+        // Координаты метки
+        [55.857508, 37.583203] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/logo-img.svg',  // картинка иконки
+            iconImageSize: [40, 54],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    // Добавление метки на карту
+    myMap.geoObjects.add(myPlacemark);
+
+    //Элементы управления
+    myMap.controls
+    // Кнопка изменения масштаба
+        .add('zoomControl')
+        // Список типов карты
+        .add('typeSelector')
+        // Кнопка изменения масштаба - справа
+        .add('smallZoomControl', { right: 5, top: 75 })
+        // Стандартный набор кнопок
+        .add('mapTools')
+        //Линейка масштаба
+        .add(new ymaps.control.ScaleLine());
+}
